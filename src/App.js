5bf50d6a9a91576b4c324b1c05cf07555feac665
Shreshth  Juneja <INTERNAL\shreshth.j@shreshth.internal.talview.com>
2020-03-13 16:15:01 +0530

    ui imroved

--- src/App.js
+++ src/App.js
@@ -4,62 +4,58 @@ import HelloWorld from "./components/HelloWorld";
 import Select from "./components/Select";
 import Div from "./components/Div";
 import Input from "./components/Input";
-import Toolbox from "./components/Toolbox"
+import Toolbox from "./components/Toolbox";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.setData = this.setData.bind(this)
-    
+    this.setData = this.setData.bind(this);
   }
 
   state = {
     arr: []
   };
 
-  setData(arr){
-    console.log("setData called " + JSON.stringify(arr))
+  setData(arr) {
+    console.log("setData called " + JSON.stringify(arr));
     this.setState({
-            arr: arr
-        }
-    )
-}
+      arr: arr
+    });
+  }
 
   render() {
-
-
     let data = this.state.arr.map(d => {
-      return (
-        <Div
-          id={d.id}
-          name={d.name}
-          className={d.properties.css_class}
-          elements={d.elements}
-        />
-      );
+      if (d.name === "div") {
+        return (
+          <Div
+            id={d.id}
+            name={d.name}
+            className={d.properties.css_class}
+            elements={d.elements}
+          />
+        );
+      } else if (d.name === "input") {
+        return <Input id={d.id} type={d.type} label={d.label} />;
+      }
     });
 
     return (
-      <div  className="App">
-        <div className="ui grid">
-        <div className="ui two column grid">
-          <div className="column">
-        <div id= "right-sidebar" ><Toolbox arr={this.state.arr}   setData={this.setData}/></div>
-        </div>
-        </div>
-<div className="column">
-        {data}
+    <div className="ui container">
+          <div className="ui grid">
+            
+          <div className="twelve wide column">
+            <br />
+            {data}
+            </div>
+            <div className="four wide column">
+              <div id="right-sidebar">
+                <Toolbox arr={this.state.arr} setData={this.setData} />
+              </div>
+            </div>
         </div>
         </div>
 
         
-        <br />
-        <br />
-        <br />
-       
-        
-
-      </div>
     );
   }
 }
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React from "react";
 
-const Input = (props) => {
-    const aStyle = {
-        
-            display: "inline-block",
-            width: "500px",
-            textAlign: "center"         
-    }
-        return (
-         <p ><label style={aStyle} htmlFor={props.id}>{props.label}</label>   <input type={props.type} id={props.id} /> <button onClick={props.onClick} id={props.id+".io"} >edit</button> </p>
-        )
-    
-}
+const Input = props => {
+  const myStyle = {
+    display: "inline-block",
+    textAlign: "left"
+  };
+  const font1 = {
+        fontFamily: "Poppins"
+  };
+  return (
+    <p>
+      <label style={myStyle} htmlFor={props.id}>
+        {props.label}
+      </label>
+      <input style={font1} type={props.type} id={props.id} />
+      <button onClick={props.onClick} id={props.id + ".io"}>
+        edit
+      </button>{" "}
+    </p>
+  );
+};
 
-export default Input
+export default Input;
